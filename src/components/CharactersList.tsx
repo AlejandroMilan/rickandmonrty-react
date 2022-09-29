b499c06887deea7ec3dc8a4265254e24ce889db7
Alejandro Milán <alejandro.milan.serrano@gmail.com>
2022-09-28 21:04:31 -0500

    [FEATURE] Character list transitions

--- src/components/CharactersList.tsx
+++ src/components/CharactersList.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { useSelector } from "react-redux";
 
+import ReactCSSTransitionGroup from "react-addons-css-transition-group";
+
 import { Character } from "../services/Characters.service";
 import CharacterCard from "./CharacterCard";
 
@@ -8,10 +10,21 @@ const CharacterList = () => {
   const { characters } = useSelector((state: any) => state);
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 gap-y-10">
-      {characters.map((character: Character) => (
-        <CharacterCard key={character.id} character={character} />
-      ))}
+    <div className="">
+      <ReactCSSTransitionGroup
+        transitionName="fade"
+        transitionAppear={true}
+        transitionAppearTimeout={500}
+        transitionEnterTimeout={500}
+        transitionLeaveTimeout={500}
+        transitionEnter={true}
+        transitionLeave={true}
+        className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 gap-y-10"
+      >
+        {characters.map((character: Character) => (
+          <CharacterCard key={character.id} character={character} />
+        ))}
+      </ReactCSSTransitionGroup>
     </div>
   );
 };
